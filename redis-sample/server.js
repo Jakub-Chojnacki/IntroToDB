31fc8cdc7696201787b1b10b55d3274e66b58e46
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2024-10-19 20:27:26 +0200

    added a simple mock caching

--- redis-sample/server.js
+++ redis-sample/server.js
@@ -5,6 +5,39 @@ const redis = require("redis");
 const client = redis.createClient();
 
 const rIncr = promisify(client.incr).bind(client);
+const rGet = promisify(client.get).bind(client);
+const rSetex = promisify(client.setex).bind(client);
+
+const cache = (key, ttl, slowFn) => {
+  return async function cachedFn(...props) {
+    const cachedResponse = await rGet(key);
+    if (cachedResponse) {
+      return cachedResponse;
+    }
+
+    const result = await slowFn(...props);
+    await rSetex(key, ttl, result);
+
+    return result;
+  };
+};
+
+async function verySlowAndExpensivePostgreSQLQuery() {
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(new Date().toUTCString());
+    }),
+      5000;
+  });
+
+  return promise;
+}
+
+const cachedFn = cache(
+  "expensive_call",
+  10,
+  verySlowAndExpensivePostgreSQLQuery
+);
 
 const init = async () => {
   const app = express();
@@ -16,6 +49,17 @@ const init = async () => {
       status: "ok",
       views,
     });
+  });
+
+  app.get("/get", async (req, res) => {
+    const data = await cachedFn();
+
+    res
+      .json({
+        data,
+        status: "ok",
+      })
+      .end();
   });
 
   const PORT = 3000;
